737840a41191e077674b6e259ab9f508e5adcf45
Do Trung Kien <do.trung.kien@framgia.com>
2013-06-07 08:30:54 +0700

    Initial Commit

---
+++ nekohoshi/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  nekohoshiAppController.h
+//  nekohoshi
+//
+//  Created by Trung Kien Do on 13/06/07.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ nekohoshi/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  nekohoshiAppController.h
+//  nekohoshi
+//
+//  Created by Trung Kien Do on 13/06/07.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
